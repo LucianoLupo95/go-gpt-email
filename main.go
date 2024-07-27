a94b5900703bebb9183c6e444cf39130f2ba7eca
Luciano_Lupo <lucianolupo.cursos01@gmail.com>
2024-07-27 09:02:59 +0200

    Inicialización del proyecto

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/emersion/go-imap/client"
+)
+
+func main() {
+    // Cargar las credenciales del entorno
+    username := os.Getenv("EMAIL_USERNAME")
+    password := os.Getenv("EMAIL_PASSWORD")
+
+    // Conectar al servidor IMAP
+    c, err := client.DialTLS("imap.gmail.com:993", nil) // Cambia esto según tu servidor IMAP
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer c.Logout()
+
+    // Iniciar sesión
+    if err := c.Login(username, password); err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println("Conexión IMAP exitosa!")
+}
